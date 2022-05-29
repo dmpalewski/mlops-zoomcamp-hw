381db80f99736d873f6fdb1040b68359339dc6cc
dmpalewski <dmpalewski@gmail.com>
2022-05-29 18:47:17 +0000

    q5 log hpo models

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import pickle
+from pathlib import Path
 
 import mlflow
 import numpy as np
@@ -9,8 +10,12 @@ from hyperopt.pyll import scope
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+
+
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("random-forest-hyperopt")
+
+ARTIFACTS_FOLDER = "artifacts"
 
 
 def load_pickle(filename):
@@ -24,11 +29,15 @@ def run(data_path, num_trials):
     X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
     def objective(params):
-
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_valid)
-        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        with mlflow.start_run():
+            mlflow.set_tag("model", "random-forest")
+            mlflow.log_params(params)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_valid)
+            rmse = mean_squared_error(y_valid, y_pred, squared=False)
+            mlflow.log_metric("rmse", rmse)
+            mlflow.sklearn.log_model(rf, artifact_path=ARTIFACTS_FOLDER)
 
         return {'loss': rmse, 'status': STATUS_OK}
 
