0c8d8a47dfb3d23ce5b2861bd86495dc077c5fc5
dmpalewski <dmpalewski@gmail.com>
2022-05-29 10:50:55 +0000

    implemented mlflow tracking with autologging

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -2,8 +2,16 @@ import argparse
 import os
 import pickle
 
+import mlflow
+from pathlib import Path
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
+
+HW_DIR = Path(__file__).parent
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("homework-02")
 
 
 def load_pickle(filename: str):
@@ -12,23 +20,25 @@ def load_pickle(filename: str):
 
 
 def run(data_path):
+    mlflow.sklearn.autolog()
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
 
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--data_path",
-        default="./output",
+        "--data_path", type=Path,
+        default=HW_DIR / "output",
         help="the location where the processed NYC taxi trip data was saved."
     )
     args = parser.parse_args()
