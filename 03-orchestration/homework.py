4ac5b54d20ce2fe6e3a14b11448108bec7ff5100
dmpalewski <dmpalewski@gmail.com>
2022-06-11 14:07:44 +0000

    q3 model serialization

--- 03-orchestration/homework.py
+++ 03-orchestration/homework.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from datetime import datetime, timedelta
 from pathlib import Path
+import pickle
 from typing import Union
 
 from sklearn.feature_extraction import DictVectorizer
@@ -11,11 +12,12 @@ from sklearn.metrics import mean_squared_error
 from prefect import flow, task, get_run_logger
 
 DATA_DIR = Path(__file__).parents[1] / "data"
+MODELS_DIR = Path(__file__).parents[1] / "models"
+
 
 @task
-def get_paths(date: Union[None, str]):
+def get_paths(date: datetime):
     logger = get_run_logger()
-    date = datetime.fromisoformat(date) if date else datetime.today()
     val_date = date.replace(day=1) - timedelta(days=1)
     train_date = val_date.replace(day=1) - timedelta(days=1)
     val_path = DATA_DIR / f"fhv_tripdata_{val_date.strftime('%Y-%m')}.parquet"
@@ -76,10 +78,11 @@ def run_model(df, categorical, dv, lr):
     return
 
 @flow
-def main(date: str="2021-08-15"):
-
+def main(date: Union[None, str]="2021-08-15"):
+    logger = get_run_logger()
+    date_obj = datetime.fromisoformat(date) if date else datetime.today()
     categorical = ['PUlocationID', 'DOlocationID']
-    train_path, val_path = get_paths(date).result()
+    train_path, val_path = get_paths(date_obj).result()
 
     df_train = read_data(train_path)
     df_train_processed = prepare_features(df_train, categorical)
@@ -89,6 +92,14 @@ def main(date: str="2021-08-15"):
 
     # train the model
     lr, dv = train_model(df_train_processed, categorical).result()
+    trained_model_path = MODELS_DIR / f"model-{date_obj.strftime('%Y-%m-%d')}.bin"
+    trained_vectorizer_path = MODELS_DIR / f"dv-{date_obj.strftime('%Y-%m-%d')}.b"
+    with open(trained_model_path, "wb") as fout:
+        pickle.dump(lr, fout)
+    logger.info(f"Model saved to {trained_model_path}")
+    with open(trained_vectorizer_path, "wb") as fout:
+        pickle.dump(dv, fout)
+    logger.info(f"DictVectorizer saved to {trained_vectorizer_path}")
     run_model(df_val_processed, categorical, dv, lr)
 
 main("2021-08-15")
